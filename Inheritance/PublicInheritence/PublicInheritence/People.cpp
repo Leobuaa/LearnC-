aafac0e1ad015fe36c7c2436c90667270bec69c0
Leo Peng <pengzimaobuaa@163.com>
2016-04-14 00:09:56 +0800

    Add one example of Inheritence

--- Inheritance/PublicInheritence/PublicInheritence/People.cpp
+++ Inheritance/PublicInheritence/PublicInheritence/People.cpp
@@ -6,4 +6,56 @@
 //  Copyright © 2016 Leo Peng. All rights reserved.
 //
 
-#include <stdio.h>
+#include <iostream>
+#include "People.h"
+
+using namespace std;
+
+People::People(const string & first, const string & last, int id, BirthDay day) {
+    firstName = first;
+    lastName = last;
+    idNum = id;
+    birthday = day;
+}
+
+void People::setFirstName(const string & first) {
+    firstName = first;
+}
+
+void People::setLastName(const string & last) {
+    lastName = last;
+}
+
+void People::setIdNum(int id) {
+    idNum = id;
+}
+
+void People::setBirthDay(const BirthDay day) {
+    birthday = day;
+}
+
+void People::print() {
+    cout << "Name: " << getName() << endl;
+    cout << "ID: " << getIdNum() << endl;
+    cout << "BirthDay: " << getBirthDay().year << '-' << getBirthDay().month << '-' << getBirthDay().day << endl;
+}
+
+string People::getName() {
+    return getFirstName() + " " + getLastName();
+}
+
+string People::getFirstName() {
+    return firstName;
+}
+
+string People::getLastName() {
+    return lastName;
+}
+
+BirthDay People::getBirthDay() {
+    return birthday;
+}
+
+int People::getIdNum() {
+    return idNum;
+}
--- Inheritance/PublicInheritence/PublicInheritence/Student.cpp
+++ Inheritance/PublicInheritence/PublicInheritence/Student.cpp
@@ -6,4 +6,19 @@
 //  Copyright © 2016 Leo Peng. All rights reserved.
 //
 
-#include "Student.hpp"
+#include "Student.h"
+
+using namespace std;
+
+Student::Student(const string & first, const string & last, int id, BirthDay day, const string & sname):People(first, last, id, day) {
+    schoolName = sname;
+}
+
+void Student::setSchoolName(const string & name){
+    schoolName = name;
+}
+
+void Student::print() {
+    People::print();
+    cout << "SchoolName: " << schoolName << endl;
+}
